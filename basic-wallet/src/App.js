511e8c9b6ffc593b02be54c940ddedc48d2ea2f9
Ang Xu <angxu1234@gmail.com>
2019-10-31 19:21:51 -0500

    mvp

--- basic-wallet/src/App.js
+++ basic-wallet/src/App.js
@@ -1,21 +1,102 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-	const [user, setUser] = useState();
-	const [users, setUsers] = useState([]);
+	const [user, setUser] = useState("None Selected");
+	const [chain, setChain] = useState([]);
+	const [values, setValues] = useState({});
 
-	useEffect(() => {
-		setUser("Default");
-		setUsers([...users, "Default"]);
-	});
+	async function getChain(e) {
+		e.preventDefault();
+		try {
+			const request = await axios.get("http://localhost:5000/chain");
+			setChain(request.data.chain);
+		} catch (error) {
+			console.log(error);
+			setChain([]);
+		}
+	}
 
-	const getChain = () => {};
+	const handleChange = e => {
+		e.persist();
+		setValues(values => ({ ...values, [e.target.name]: e.target.value }));
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		setUser(values.user);
+	};
+
+	const balance = chain.reduce(
+		(a, c) =>
+			a +
+			c.transactions.filter(trn => trn.recipient === user).length -
+			c.transactions.filter(trn => trn.sender === user).length,
+		0
+	);
+
+	const transactions = chain.reduce(
+		(a, c) =>
+			a.concat(
+				c.transactions.filter(
+					trn => trn.recipient === user || trn.sender === user
+				)
+			),
+		[]
+	);
 
 	return (
 		<div className="App">
-			<p>Welcome</p>
-			<button onSubmit={getChain}>Check Chain</button>
+			<p>Welcome, Please Retrieve Complete Chain First</p>
+			<p>Current User: {user}</p>
+			<p>Balance: {balance} </p>
+			<div>
+				Transactions:
+				{transactions.map((trn, index) => {
+					return (
+						<div className="transaction" key={index}>
+							*sender: {trn.sender}, recipient: {trn.recipient}, amount:{" "}
+							{trn.amount}*
+						</div>
+					);
+				})}
+			</div>
+			<form onSubmit={handleSubmit}>
+				<label>
+					User:
+					<input
+						type="text"
+						name="user"
+						value={values.user || ""}
+						onChange={handleChange}
+					/>
+					<input type="submit" value="Submit" />
+				</label>
+			</form>
+			<button>Check Transactions for User</button>
+			<button onClick={getChain}>Retrieve Complete Chain</button>
+			<div className="chain">
+				{chain.map(block => {
+					return (
+						<div className="block" key={block.index}>
+							<p>index: {block.index}</p>
+							<p>previous hash: {block.previous_hash}</p>
+							<p>proof: {block.proof}</p>
+							<p>timestamp: {block.timestamp}</p>
+							transactions:{" "}
+							{block.transactions.map((trn, index) => {
+								return (
+									<div className="transaction" key={index}>
+										*sender: {trn.sender}, recipient: {trn.recipient}, amount:{" "}
+										{trn.amount}*
+									</div>
+								);
+							})}
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
